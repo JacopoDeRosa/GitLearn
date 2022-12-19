24d3f1b9685c73e328009e890d277b51e52a9e39
Jack <jacopo.derosa00@gmail.com>
2022-12-19 10:24:44 +0100

    Initial Commit

---
+++ Source/GitLearn/GitLearn.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GitLearn.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GitLearn, "GitLearn" );
+ 
---
+++ Source/GitLearn/GitLearnGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GitLearnGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGitLearnGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGitLearnGameMode();
+};
+
+
+
